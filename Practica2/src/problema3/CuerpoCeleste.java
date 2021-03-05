e93a4ceef483fd6d0326a374910de112d6816e55
HP <HP@DESKTOP-KO32DFN>
2021-03-04 23:36:16 -0600

    argumeno

---
+++ Practica2/src/problema3/CuerpoCeleste.java
@@ -0,0 +1,110 @@
+/*******************************************************************************
+ * 2021, All rights reserved.
+ *******************************************************************************/
+package problema3;
+
+import problema3.Ubicación;
+// Start of user code (user defined imports)
+
+// End of user code
+
+/**
+ * Description of CuerpoCeleste.
+ * 
+ * @author HP
+ */
+public class CuerpoCeleste {
+	/**
+	 * Description of the property nombre.
+	 */
+	private String nombre = "";
+
+	/**
+	 * Description of the property localizaciones.
+	 */
+	private Ubicació
+	n localizaciones = null;
+
+	/**
+	 * Description of the property composicion.
+	 */
+	private String composicion = "";
+
+	// Start of user code (user defined attributes for CuerpoCeleste)
+
+	// End of user code
+
+	/**
+	 * The constructor.
+	 */
+	public CuerpoCeleste() {
+		// Start of user code constructor for CuerpoCeleste)
+		super();
+		// End of user code
+	}
+
+	/**
+	 * Description of the method desplazamiento.
+	 * @param i 
+	 * @param j 
+	 * @return 
+	 */
+	public boolean desplazamiento(int i, int j) {
+		// Start of user code for method desplazamiento
+		boolean desplazamiento = false;
+		return desplazamiento;
+		// End of user code
+	}
+
+	// Start of user code (user defined methods for CuerpoCeleste)
+
+	// End of user code
+	/**
+	 * Returns nombre.
+	 * @return nombre 
+	 */
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	/**
+	 * Sets a value to attribute nombre. 
+	 * @param newNombre 
+	 */
+	public void setNombre(String newNombre) {
+		this.nombre = newNombre;
+	}
+
+	/**
+	 * Returns localizaciones.
+	 * @return localizaciones 
+	 */
+	public Ubicación getLocalizaciones() {
+		return this.localizaciones;
+	}
+
+	/**
+	 * Sets a value to attribute localizaciones. 
+	 * @param newLocalizaciones 
+	 */
+	public void setLocalizaciones(Ubicación newLocalizaciones) {
+	    this.localizaciones = newLocalizaciones;
+	}
+
+	/**
+	 * Returns composicion.
+	 * @return composicion 
+	 */
+	public String getComposicion() {
+		return this.composicion;
+	}
+
+	/**
+	 * Sets a value to attribute composicion. 
+	 * @param newComposicion 
+	 */
+	public void setComposicion(String newComposicion) {
+		this.composicion = newComposicion;
+	}
+
+}
---
+++ Practica2/src/problema3/Ubicación.java
@@ -0,0 +1,117 @@
+/*******************************************************************************
+ * 2021, All rights reserved.
+ *******************************************************************************/
+package problema3;
+
+// Start of user code (user defined imports)
+
+// End of user code
+
+/**
+ * Description of Ubicación.
+ * 
+ * @author HP
+ */
+public class Ubicación
+{
+	/**
+	 * Description of the property longitud.
+	 */
+	private float longitud = 0F;
+
+	/**
+	 * Description of the property latitud.
+	 */
+	private float latitud = 0F;
+
+	/**
+	 * Description of the property periodo.
+	 */
+	private String periodo = "";
+
+	/**
+	 * Description of the property distancia.
+	 */
+	private float distancia = 0F;
+
+	// Start of user code (user defined attributes for Ubicación)
+
+	// End of user code
+
+	/**
+	 * The constructor.
+	 */
+	public Ubicación() {
+		// Start of user code constructor for Ubicación)
+		super();
+		// End of user code
+	}
+
+	// Start of user code (user defined methods for Ubicación)
+
+	// End of user code
+	/**
+	 * Returns longitud.
+	 * @return longitud 
+	 */
+	public float getLongitud() {
+		return this.longitud;
+	}
+
+	/**
+	 * Sets a value to attribute longitud. 
+	 * @param newLongitud 
+	 */
+	public void setLongitud(float newLongitud) {
+		this.longitud = newLongitud;
+	}
+
+	/**
+	 * Returns latitud.
+	 * @return latitud 
+	 */
+	public float getLatitud() {
+		return this.latitud;
+	}
+
+	/**
+	 * Sets a value to attribute latitud. 
+	 * @param newLatitud 
+	 */
+	public void setLatitud(float newLatitud) {
+		this.latitud = newLatitud;
+	}
+
+	/**
+	 * Returns periodo.
+	 * @return periodo 
+	 */
+	public String getPeriodo() {
+		return this.periodo;
+	}
+
+	/**
+	 * Sets a value to attribute periodo. 
+	 * @param newPeriodo 
+	 */
+	public void setPeriodo(String newPeriodo) {
+		this.periodo = newPeriodo;
+	}
+
+	/**
+	 * Returns distancia.
+	 * @return distancia 
+	 */
+	public float getDistancia() {
+		return this.distancia;
+	}
+
+	/**
+	 * Sets a value to attribute distancia. 
+	 * @param newDistancia 
+	 */
+	public void setDistancia(float newDistancia) {
+		this.distancia = newDistancia;
+	}
+
+}
